3fd05d475ed414a4e524bfc8f763f8ed9353dcfe
dawidkaszuba <d.kaszuba89@gmail.com>
2025-08-13 20:32:28 +0200

    commit before changge PC

--- db/changelog/2025_07_29_create_basic_tables/create_basic_tables.sql
+++ db/changelog/2025_07_29_create_basic_tables/create_basic_tables.sql
@@ -1,14 +1,25 @@
-CREATE TABLE measurements (
-    ME_ID BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    ME_DEVICE_ID VARCHAR(50) NOT NULL,
-    ME_TIMESTAMP TIMESTAMPTZ NOT NULL,
-    ME_METRIC_NAME VARCHAR(100) NOT NULL,
-    ME_METRIC_VALUE DOUBLE PRECISION,
-    ME_UNIT VARCHAR(20)
+CREATE TABLE devices (
+    de_id SERIAL PRIMARY KEY,
+    de_uid VARCHAR(64) UNIQUE NOT NULL,
+    de_name VARCHAR(255),
+    de_ip VARCHAR(15),
+    de_last_seen TIMESTAMP,
+    de_delete_time TIMESTAMP,
+    de_change_time TIMESTAMP DEFAULT NOW(),
+    de_change_user VARCHAR(64)
+);
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE devices TO iot_user;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE devices_de_id_seq TO iot_user;
+
+CREATE TABLE sensors (
+    se_id SERIAL PRIMARY KEY,
+    de_id INT NOT NULL REFERENCES devices(de_id) ON DELETE CASCADE,
+    se_type VARCHAR(50) NOT NULL,
+    se_unit VARCHAR(20) NOT NULL,
+    se_change_time TIMESTAMP DEFAULT NOW(),
+    se_change_user VARCHAR(64),
+    se_delete_time TIMESTAMP
 );
 
-CREATE INDEX idx_measurements_device_time
-    ON measurements (ME_DEVICE_ID, ME_TIMESTAMP DESC);
-
-CREATE INDEX idx_measurements_metric
-    ON measurements (ME_METRIC_NAME);
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE sensors TO iot_user;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE sensors_se_id_seq TO iot_user;
--- db/changelog/2025_07_29_create_basic_tables/rollback_create_basic_tables.sql
+++ db/changelog/2025_07_29_create_basic_tables/rollback_create_basic_tables.sql
@@ -1,1 +1,2 @@
-DROP TABLE measurements;
+DROP TABLE sensors cascade;
+DROP TABLE devices cascade;
